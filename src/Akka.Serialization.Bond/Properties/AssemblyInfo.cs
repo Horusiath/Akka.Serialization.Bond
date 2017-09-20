16e82a04ed232a834343c1a155852da275910d40
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2017-09-20 21:07:48 +0200

    added nuspec

--- src/Akka.Serialization.Bond/Properties/AssemblyInfo.cs
+++ src/Akka.Serialization.Bond/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Akka.Serialization.Bond")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Akka.NET integration plugin for Microsoft Bond serizalier")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Akka.Serialization.Bond")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Bartosz Sypytkowski 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: InternalsVisibleTo("Akka.Serialization.Bond.Tests")]
